2f1e10364f9ce7c990470a0366562c05794258d8
GonzaLuzC <gonzalobernardez@luzconsulting.com.ar>
2026-05-22 12:30:33 -0300

    Configuración inicial

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contablelegal-Test</title>
+</head>
+
+<body>
+    <article>
+        <h1>Contablelegal-Test</h1>
+        <p>Este es un sitio de prueba para testear funcionalidades e integraciones básicas</p>
+    </article>
+</body>
+
+</html>
